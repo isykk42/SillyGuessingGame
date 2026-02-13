e5556b55610216bfdea21b5ec968c2e7d041e196
thanh <ttran112@charlotte.edu>
2026-02-13 18:17:21 -0500

    finished and finalized and formatted

--- GoodGuessing.py
+++ GoodGuessing.py
@@ -5,24 +5,31 @@ def play_guessing_game():
     guess = 0
     attempts = 0
 
-    print("Welcome to the (good code) Guessing Game !!!")
-    print("\nA number has been chosen randomly between 1 and 25! You have 25 guesses and good luck!")
+    print("\nWelcome to the (good code) Guessing Game !!!")
+    print("A number has been chosen randomly between 1 and 25! You have 25 guesses and good luck!")
 
-    while guess != randomnumber:
+    while guess != randomnumber and attempts < 25:
         try:
             guess = int(input("\nGuess a number between 1 and 25: "))
             attempts += 1
 
             if guess < randomnumber:
-                print("\nToo low, try again!")
+                print("Too low, try again!")
 
             elif guess > randomnumber:
-                print("\nToo high, try again!")
+                print("Too high, try again!")
+        except ValueError:
+            print("\nPlease enter a number between 1 and 25!")
+
+    if guess == randomnumber:
+        print(f"\nCongratulations! You guessed the number {randomnumber} in {attempts} attempts!")
+
+    else:
+        print("\nYou have ran out of attempts :(")
+        print("The number was " + str(randomnumber))
 
 
-        finally:
-            if attempts > 25:
-                print("\nYou have ran out of attempts")
-                print("\nThe number was " + str(randomnumber))
+    print("Thank you for playing!")
 
-
+if __name__ == "__main__":
+    play_guessing_game()
